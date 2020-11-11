51b7f922f29495c730a3a9a7937eb41397b71a27
PragatiVerma18 <itispragativerma@gmail.com>
2020-11-12 02:02:45 +0530

    Added Projects section

---
+++ src/Components/Projects/ProjectCard.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Projects from "./data.json";
+
+function ProjectCard() {
+  return (
+    <>
+      {Projects.map((el) => {
+        return (
+          <div class="col-sm-6 col-lg-6 col-xl-4">
+            <div class="card" key={el.id}>
+              <img class="card-img-top" src={el.image} alt={el.title} />
+              <div class="card-body">
+                <h4 class="card-title">{el.title}</h4>
+                <p class="card-text">{el.tagline}</p>
+                <div className="icon-tray">
+                  <span className="icon">
+                    <a href={el.devpost}>
+                      <i className="fab fa-github fa-2x m-3"></i>
+                    </a>
+                  </span>
+                  <span className="icon">
+                    <a href={el.youtube}>
+                      <i className="fab fa-youtube fa-2x m-3"></i>
+                    </a>
+                  </span>
+                </div>
+                <a
+                  href={el.devpost}
+                  class="btn btn-block btn-lg btn-primary mt-4"
+                >
+                  View More
+                </a>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
+}
+
+export default ProjectCard;
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ProjectCard from "./ProjectCard";
 
 function Projects() {
   return (
@@ -6,6 +7,11 @@ function Projects() {
       <h1 className="text-center">
         <strong>Projects</strong>
       </h1>
+      <div class="container">
+        <div class="row">
+          <ProjectCard />
+        </div>
+      </div>
     </div>
   );
 }
